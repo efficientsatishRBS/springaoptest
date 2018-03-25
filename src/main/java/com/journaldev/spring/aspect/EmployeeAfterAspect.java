cf0a6fd0f09063ceb3f1f8c9db440c731ac535af
Satish Kumar <efficient.satish@gmail.com>
2018-03-25 20:20:13 +0530

    commit on test branch

--- src/main/java/com/journaldev/spring/aspect/EmployeeAfterAspect.java
+++ src/main/java/com/journaldev/spring/aspect/EmployeeAfterAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
+//this code looks good
 @Aspect
 public class EmployeeAfterAspect {
 
